0a723d8228bb8ea8396e161243a3d7159a4f342a
Bittu <kumarbittu775@gmail.com>
2023-12-30 15:56:10 +0530

    jn

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ const App = () => {
     <div>
       <Navbar />
       <Routes>
-        <Route exact path="/Reva_web/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
 
         <Route path="/Reva_web/achievements" element={<AchievementPage />} />
         <Route path="/Reva_web/Gallery" element={<Gallery />} />
