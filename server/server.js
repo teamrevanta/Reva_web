9f353202e9c5642d63713aa47729cf0fc8c2b2a7
Anjeet Singh <anjeetsingh842310@gmail.com>
2025-07-06 20:43:06 +0530

    Add backend server setup without node_modules

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
